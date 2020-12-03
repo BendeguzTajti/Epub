a2853a0598d217b2f7c8d88c6db31a02b3ff01c2
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-03 15:50:08 +0100

    add constants

--- app/src/main/java/com/codecool/epub/util/Constants.kt
+++ app/src/main/java/com/codecool/epub/util/Constants.kt
@@ -5,13 +5,16 @@ class Constants {
     companion object {
 
         // API RELATED
-        const val BASE_URL = "https://id.twitch.tv/"
+        const val BASE_URL = "https://api.twitch.tv/"
+        const val AUTH_ENDPOINT = "//id.twitch.tv/oauth2/token"
+
         const val CLIENT_ID = "YOUR_CLIENT_ID"
         const val CLIENT_SECRET = "YOUR_CLIENT_SECRET"
 
+        const val GRANT_TYPE = "client_credentials"
+
         // SHARED PREFERENCES RELATED
         const val SHARED_PREF_NAME = "EPUB_SHARED_PREFERENCES"
-        const val ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY"
-        const val TOKEN_EXPIRATION_KEY = "TOKEN_EXPIRATION_KEY"
+        const val TOKEN_KEY = "TOKEN_KEY"
     }
 }
