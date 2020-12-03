b6c0782c43d3d7788c8c74e115e75dceb123f874
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-03 17:39:09 +0100

    remove unnecessary classes and methods

--- app/src/main/java/com/codecool/epub/Modules.kt
+++ app/src/main/java/com/codecool/epub/Modules.kt
@@ -1,32 +1,41 @@
 package com.codecool.epub
 
 import android.content.Context
-import com.codecool.epub.api.TwitchApi
-import com.codecool.epub.repository.Repository
+import android.content.SharedPreferences
+import com.codecool.epub.api.AuthApi
 import com.codecool.epub.util.Constants.Companion.BASE_URL
 import com.codecool.epub.util.Constants.Companion.SHARED_PREF_NAME
-import com.codecool.epub.viewmodel.AuthViewModel
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val appModules = module {
 
-    single { androidContext().getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE) }
-    single { provideRetrofit() }
+    // NETWORK
+    single { provideOkHttpClient() }
+    single { provideRetrofit(get()) }
     factory { provideTwitchApi(get()) }
-    single { Repository(get(), get()) }
-    viewModel { AuthViewModel(get()) }
+
+    single { provideSharedPreferences(androidContext()) }
 
 }
 
-fun provideRetrofit(): Retrofit {
+fun provideSharedPreferences(context: Context): SharedPreferences {
+    return context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+}
+
+fun provideOkHttpClient(): OkHttpClient {
+    return OkHttpClient()
+}
+
+fun provideRetrofit(client: OkHttpClient): Retrofit {
     return Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
 
-fun provideTwitchApi(retrofit: Retrofit): TwitchApi = retrofit.create(TwitchApi::class.java)
+fun provideTwitchApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
