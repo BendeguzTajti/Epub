4339919726a89c99ed7f977ebacf70fe203a9990
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-07 09:20:34 +0100

    move HomeFragment to view package

---
+++ app/src/main/java/com/codecool/epub/view/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.codecool.epub
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        search_fab.setOnClickListener {
+            Log.d("kaka", "onViewCreated: KAKA")
+        }
+//        val navController = findNavController()
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        topAppBar.setupWithNavController(navController, appBarConfiguration)
+    }
+}
