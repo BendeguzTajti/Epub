98d719c2aee0d25acf590ba2b2da922dabe3c12b
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-12 05:39:44 +0100

    create StreamsResponse model

---
+++ app/src/main/java/com/codecool/epub/model/StreamsResponse.kt
@@ -0,0 +1,26 @@
+package com.codecool.epub.model
+
+import com.google.gson.annotations.SerializedName
+
+data class StreamsResponse(val data: List<Stream>) {
+
+    data class Stream(
+        val id: String,
+        @SerializedName("user_id")
+        val userId: String,
+        @SerializedName("user_name")
+        val userName: String,
+        @SerializedName("game_id")
+        val gameId: String,
+        @SerializedName("game_name")
+        val gameName: String,
+        val type: String,
+        val title: String,
+        @SerializedName("viewer_count")
+        val viewerCount: Int,
+        @SerializedName("thumbnail_url")
+        private val thumbnailUrl: String
+    ) {
+        fun getThumbnailUrl(width: Int, height: Int): String = thumbnailUrl.replace("{width}", "$width").replace("{height}", "$height")
+    }
+}
