a404799557b8900ae641093c8fbe8991e856931b
gabona07 <bonagabriella1995@gmail.com>
2020-12-14 07:13:10 +0100

    Add new API call

--- app/src/main/java/com/codecool/epub/network/api/DataApi.kt
+++ app/src/main/java/com/codecool/epub/network/api/DataApi.kt
@@ -1,11 +1,18 @@
 package com.codecool.epub.network.api
 
 import com.codecool.epub.model.GamesResponse
+import com.codecool.epub.model.VideoResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface DataApi {
 
     @GET("helix/games/top")
     suspend fun getTopGames(): Response<GamesResponse>
+
+
+    @GET("helix/videos?")
+    suspend fun getVideosByGameId(@Query("game_id") game_id: String): Response<VideoResponse>
 }
