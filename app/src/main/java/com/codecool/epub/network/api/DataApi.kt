09740e416f4ff230974c1fbc9e9c3d2e3d79256d
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-14 04:22:01 +0100

    limit recommended categories to 8

--- app/src/main/java/com/codecool/epub/network/api/DataApi.kt
+++ app/src/main/java/com/codecool/epub/network/api/DataApi.kt
@@ -3,9 +3,10 @@ package com.codecool.epub.network.api
 import com.codecool.epub.model.GamesResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface DataApi {
 
     @GET("helix/games/top")
-    suspend fun getTopGames(): Response<GamesResponse>
+    suspend fun getTopGames(@Query("first") limit: Int = 8): Response<GamesResponse>
 }
