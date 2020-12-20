2e5de27ccff39d39bb2c24ca30df4c8fcec2f978
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-20 16:21:34 +0100

    create Recommendation sealed class

---
+++ app/src/main/java/com/codecool/epub/model/Recommendation.kt
@@ -0,0 +1,13 @@
+package com.codecool.epub.model
+
+import java.lang.Exception
+
+sealed class Recommendation{
+
+    data class OnSuccess(val topStreamsResponse: StreamsResponse,
+                         val topCategories: GamesResponse,
+                         val recommendedStreams1: StreamsResponse,
+                         val recommendedStreams2: StreamsResponse) : Recommendation()
+
+    data class OnError(val exception: Exception) : Recommendation()
+}
