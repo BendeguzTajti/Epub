0c726b3f5658b99673ba178a4eee572c9f3d09e1
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-12-22 03:23:43 +0100

    implement viewBinding in StreamFragment

---
+++ app/src/main/java/com/codecool/epub/view/ui/StreamFragment.kt
@@ -0,0 +1,31 @@
+package com.codecool.epub.view.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codecool.epub.databinding.FragmentStreamBinding
+
+class StreamFragment : Fragment() {
+
+    private var _binding: FragmentStreamBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStreamBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
